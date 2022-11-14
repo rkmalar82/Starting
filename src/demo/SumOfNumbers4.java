e30fcc160fd10332c68946a37ae5043b33031f6b
rkmalar82 <rkmalar@gmail.com>
2022-11-14 12:05:59 +0530

    Start

---
+++ src/demo/SumOfNumbers4.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class SumOfNumbers4  
+{  
+public static void main(String args[])  
+{  
+//int x = Integer.parseInt(args[0]); //first arguments   
+//int y = Integer.parseInt(args[1]); //second arguments  
+
+
+int x=5;
+int y = 12;
+
+
+int sum = x + y;  
+System.out.println("The sum of x and y is: " +sum);  
+}  
+}  
